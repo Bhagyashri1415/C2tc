6a5e05577c35ab81a7b475e945149accc6a431a8
BHAGYASHRI M <BHAGYASHRI M@LAPTOP-IO8O6SLU>
2022-11-30 15:11:43 +0530

    Learning basic

--- FirstC2tc/src/c2tcDemo/Demo.java
+++ FirstC2tc/src/c2tcDemo/Demo.java
@@ -5,6 +5,9 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HEllo!!");
+		System.out.println("Hii");
+		int a=5;
+		System.out.println(a);
 
 	}
 
